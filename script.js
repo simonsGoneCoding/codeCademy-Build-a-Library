c51f45664ba8f8e525f842db21743d327f769e68
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-16 20:51:58 +0000

    Create Book and Movie classes.

--- script.js
+++ script.js
@@ -41,6 +41,32 @@ class Media {
   }
 }
 
-// const book = new Media("Tomek i jego ziomek");
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
 
-// console.log(book)
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+  get runTime() {
+    return this._runTime;
+  }
+}
