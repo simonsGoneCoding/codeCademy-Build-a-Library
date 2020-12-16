145598def70b68203cfcc2140cb21ea6b62ad532
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-16 20:35:50 +0000

    Create Media class with getters, setters and some methods.

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 Create a parent class named Media with three subclasses: Book, Movie, and CD. These three subclasses have the following properties and methods:
 
-##Book
+## Book
 
 Properties: author (string), title (string), pages (number), isCheckedOut (boolean, initially false), and ratings (array, initially empty).
 Getters: all properties have a getter
 Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
 
-##Movie
+## Movie
 
 Properties: director (string), title (string), runTime (number), isCheckedOut (boolean, initially false), and ratings (array, initially empty)
 Getters: all properties have a getter
 Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
 
-##CD
+## CD
 
 Properties: artist (string), title (string), isCheckedOut (boolean, initially false), and ratings (array, initially empty), songs (array of strings)
 Getters: all properties have a getter
--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-class Media {}
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  // ----- getters -----
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+
+  //----- setters -----
+  set isCheckedOut(value) {
+    if (typeof value === "boolean") {
+      this._isCheckedOut = value;
+    } else {
+      console.log("Wrong type!");
+    }
+  }
+
+  //----- methods -----
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+
+  getAverageRating() {
+    const averageRate =
+      this._ratings.reduce((a, b) => a + b) / this._ratings.length;
+    return averageRate;
+  }
+
+  addRating(rate) {
+    this._ratings.push(rate);
+  }
+}
+
+// const book = new Media("Tomek i jego ziomek");
+
+// console.log(book)
