70ebe88db4e958bcbe46c521b357ab8774c095cb
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-16 20:57:47 +0000

    Create instances for both Book and Movie classes.

--- script.js
+++ script.js
@@ -70,3 +70,27 @@ class Movie extends Media {
     return this._runTime;
   }
 }
+
+const historyOfEverything = new Book(
+  "A Short History of Nearly Everything",
+  "Bill Boryson",
+  544
+);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie("Speed", "Jan de Bont", 116);
+
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(3);
+console.log(speed.getAverageRating());
